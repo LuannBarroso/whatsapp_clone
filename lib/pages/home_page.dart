003521834ed8b9786da5ef9ab108909ebe4089a3
LuannBarroso <luan23.lba@gmail.com>
2023-04-25 22:24:38 -0400

    alter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,45 +8,13 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 66, 62, 62),
-          title: const Text(
-            'Whatsapp',
-            style: TextStyle(
-              color: Color.fromARGB(255, 117, 122, 122),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(70),
+          child: AppBar(
+            elevation: 0,
+            title: const Padding(
+              padding: EdgeInsets.only(top: 10),
             ),
-          ),
-          actions: [
-            Row(
-              children: const [
-                Icon(Icons.camera_alt_outlined),
-                SizedBox(
-                  width: 20,
-                ),
-                Icon(Icons.search),
-                SizedBox(
-                  width: 20,
-                ),
-                Icon(Icons.more_vert_rounded),
-              ],
-            ),
-          ],
-          bottom: const TabBar(
-            labelColor: Color.fromARGB(255, 117, 122, 122),
-            tabs: [
-              Tab(
-                icon: Icon(Icons.people),
-              ),
-              Tab(
-                text: 'Conversas',
-              ),
-              Tab(
-                text: 'Status',
-              ),
-              Tab(
-                text: 'Chamadas',
-              ),
-            ],
           ),
         ),
         backgroundColor: const Color.fromARGB(255, 31, 32, 31),
